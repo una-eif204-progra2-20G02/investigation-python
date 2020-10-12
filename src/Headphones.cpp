3429807a0602b02459dbf6e03d727a161a26dc78
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2020-10-11 23:07:54 -0600

    Create Headphones.cpp

---
+++ src/Headphones.cpp
@@ -0,0 +1,16 @@
+//
+// Created by diana on 11/10/2020.
+//
+
+#include "Headphones.h"
+
+string Headphones::toString() {
+    stringstream output;
+    output << "Headphones" << endl;
+    return output.str();
+}
+
+string Headphones::doSomething() {
+    return "I do something too";
+}
+
